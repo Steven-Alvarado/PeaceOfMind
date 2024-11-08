6feb4b0f9f1eb7cac05d70a348d492807e2c543d
anassadam <anassadam123@gmail.com>
2024-11-07 21:40:13 -0500

    Completed feature therapistProfileManagement

---
+++ backend/controllers/therapistController.js
@@ -0,0 +1,86 @@
+const bcrypt = require('bcrypt');
+const {createUser, createTherapist, findTherapistById, getAvilableTherapists } = require('../models/authModel');
+
+
+exports.registerTherapist = async (req, res) => {
+    //Some validation result
+    //const errors =
+    if (!(email && password && licenseNumber && specialization && firstName && lastName && address)) {
+        return res.status(400).json({ error: "Missing required fields" });
+    }
+    //To resgiter as a therapist you need:
+    try{
+        if (!await isLicenseVerified(licenseNumber)) {
+            return res.status(403).json({ error: "License number is not verified" });
+        }
+
+        const existingUser = await findUserByEmail(email);
+        if (existingUser) {
+            return res.status(409).json({ error: "User already exists with this email" });
+        }
+
+        const passwordHash = await bcrypt.hash(password, 10);
+        const {userId} = await createUser(email, passwordHash, 'therapist');
+        const therapist = await createTherapist(userId, licenseNumber,specialization, experienceYears, monthlyRate);
+
+        res.status(201).json({
+            message: 'Therapist registered successfully',
+            therapist: {
+                id: therapist.id,
+                userId,
+                firstName,
+                lastName,
+                email,
+                address,
+                licenseNumber,
+                specialization,
+                experienceYears,
+                monthlyRate,
+                createdAt: therapist.created_at,
+                updatedAt: therapist.updated_at
+            }
+        });
+    }
+    catch(error){
+        console.error("Error registering therapist:", error);
+        res.status(500).json({message:"Therapist registration failed", error: error.message});
+    }
+
+    
+};
+
+exports.getTherapistDetails = async (req, res) => {
+    const therapistId = req.params.id;
+    try{
+        const therapist = await findTherapistById(therapistId);
+        if(!therapist){
+            return res.status(404).json({error: "Therapist not found"});
+        }
+        res.json({
+            message: "Therapist details success",
+            therapist
+        });
+    }
+    catch(error){
+        console.error("Error therapist details:", error);
+        res.status(500).json({error: "Failed to fetch therapisr details", message: error.message});
+    }
+}
+
+exports.listAvailableTherapists = async (req, res) => {
+    try{
+        const therapists = await getAvilableTherapists();
+        if(therapists.length == 0){
+            return res.status(404).json({message: "No available therapists found"});
+        }
+        res.json({message: "Available therapists retrieved successfully", therapists});
+    }
+    catch(error){
+        console.error("Error retrieving available therapists:", error)
+        res.status(500).json({error: "Failed to fetch available therapists", message: error.message});
+    }
+};
+
+
+
+module.exports = { registerTherapist, getTherapistDetails};
--- backend/models/authmodel.js
+++ backend/models/authmodel.js
@@ -1,5 +1,14 @@
 const pool = require('../config/db'); // Pool instance for PostgreSQL
 
+//Check therapist license is valid in verified_license table
+const isLicenseVerified = async (licenseNumber) => {
+  const result = await pool.query(
+    `SELECT 1 FROM verified_licenses WHERE license_number = $1`,
+    [licenseNumber]
+  );
+  return result.rowCount > 0;
+}
+
 // Create a new user in the auth table
 const createUser = async (email, passwordHash, role) => {
   const userResult = await pool.query(
@@ -12,7 +21,17 @@ const createUser = async (email, passwordHash, role) => {
     `INSERT INTO auth (email, password_hash, user_id) VALUES ($1, $2, $3) RETURNING email, user_id`,
     [email, passwordHash, userId]
   );
-  return authResult.rows[0];
+
+  return {userId, email: authResult.rows[0].email };
+};
+
+const createTherapist = async(userId, licenseNumber, specialization, experienceYears, monthlyRate) => {
+  const therapistResult = await pool.query(
+    `INSERT INTO therapists (user_id, license_number, specialization, experience_years, monthly_rate, created_at, updated_at)
+     VALUES ($1, $2, $3, $4, $5, NOW(), NOW()) RETURNING *`,
+     [userId, licenseNumber, specialization, experienceYears, monthlyRate]
+  );
+  return therapistResult.rows[0];
 };
 
 // Find a user by email in the auth table
@@ -26,5 +45,41 @@ const findUserById = async (userId) => {
   const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [userId]);
   return result.rows[0];
 };
+//Find a therapist by id in therapist table
+const findTherapistById = async (therapistId) => {
+    const query = `
+        SELECT t.*, a.email
+        FROM therapists t
+        INNER JOIN users u ON t.user_id = u.id
+        INNER JOIN auth a ON u.id = a.user_id
+        WHERE t.id = $1;
+    `;
+    const result = await pool.query(query, [therapistId]);
+    if (result.rows.length > 0) {
+        return result.rows[0];
+    } else {
+        return null; // Return null if no therapist is found
+    }
+};
+//Get the availabe therapist using availability in therapist table
+const getAvilableTherapists = async() =>{
+  const query = `
+        SELECT t.*, a.email
+        FROM therapists t
+        INNER JOIN users u ON t.user_id = u.id
+        INNER JOIN auth a ON u.id = a.user_id
+        WHERE t.availability = true;
+    `;
+  const result = await pool.query(query);
+  return result.rows;
+};
 
-module.exports = { createUser, findUserByEmail, findUserById };
+module.exports = {
+    createUser,
+    createTherapist,
+    findUserByEmail,
+    findUserById,
+    isLicenseVerified,
+    findTherapistById,
+    getAvilableTherapists
+};
