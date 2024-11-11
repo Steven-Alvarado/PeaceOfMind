d5b1598a856bd350b3531a0dff0a94da48e3a7db
nb538 <nb538@njit.edu>
2024-11-10 22:21:50 -0500

    Completed feature therapistAppointmentRetrieval

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userRoutes = require('./users');
-const authRoutes = require('./auth');
+const userRoutes = require("./users");
+const authRoutes = require("./auth");
+const therapistRoutes = require("./therapists");
 
 // Mount routes on `/api`
-router.use('/users', userRoutes);
-router.use('/auth', authRoutes);
+router.use("/users", userRoutes);
+router.use("/auth", authRoutes);
+router.use("/therapists", therapistRoutes);
 
 module.exports = router;
-
