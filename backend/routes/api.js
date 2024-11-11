6274abcb54445ed68d2e302567f129cac354c6f6
nb538 <nb538@njit.edu>
2024-11-10 19:30:58 -0500

    Completed feature appointmentManagement

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userRoutes = require('./users');
-const authRoutes = require('./auth');
+const userRoutes = require("./users");
+const authRoutes = require("./auth");
+const appointmentRoutes = require("./appointments"); // Appointments file
 
 // Mount routes on `/api`
-router.use('/users', userRoutes);
-router.use('/auth', authRoutes);
+router.use("/users", userRoutes);
+router.use("/auth", authRoutes);
+router.use("/appointments", appointmentRoutes); // Mounts appointments routes
 
 module.exports = router;
-
