b7b546d61518317270ea5cccf18155a3430d69b0
Daniel Lobo <dzl2@njit.edu>
2024-11-16 16:25:21 -0500

    Ticket 4 Document Management Endpoints

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const userRoutes = require('./users');
 const authRoutes = require('./auth');
+const documentRoutes = require('./documents');
 
 // Mount routes on `/api`
 router.use('/users', userRoutes);
 router.use('/auth', authRoutes);
+router.use("/documents", documentRoutes);
 
 module.exports = router;
 
