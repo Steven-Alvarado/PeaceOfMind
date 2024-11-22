11ca70273df4341b553dc81d3f154d21a8bec5fe
nb538 <nb538@njit.edu>
2024-11-22 15:57:57 -0500

    In Progress Feature therapistList

--- frontend/src/pages/StudentDashboard.tsx
+++ frontend/src/pages/StudentDashboard.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from "react";
 import HeaderStudentDashboard from "../components/headerStudentDashboard";
 import Footer from "../components/Footer";
 import {
@@ -14,43 +13,223 @@ import {
   FaFileInvoice,
   FaQuestionCircle,
   FaClipboardList,
+  FaDollarSign,
+  FaBookReader,
+  FaRestroom,
 } from "react-icons/fa";
+import { FaArrowRightArrowLeft } from "react-icons/fa6";
 import { FaClipboardQuestion } from "react-icons/fa6";
 
 import Lottie from "lottie-react";
 import StudentDashboardAnimation from "../assets/lotties/StudentDashboardAnimation.json";
 
-
-
-
 // Help Modal Component
-const HelpModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
+const HelpModal = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white rounded-lg p-6 w-full max-w-lg outline outline-white outline-2 outline-offset-2">
-        <h2 className="text-3xl font-extrabold text-center text-[#5E9ED9] mb-4">Help</h2>
+        <h2 className="text-3xl font-extrabold text-center text-[#5E9ED9] mb-4">
+          Help
+        </h2>
         <ul className="text-gray-700 space-y-3">
           <li>
-            <FaBook className="inline mr-2" /> <strong>Journal:</strong> Reflect on your thoughts and emotions.
+            <FaBook className="inline mr-2" /> <strong>Journal:</strong> Reflect
+            on your thoughts and emotions.
           </li>
           <li>
-            <FaClipboardList className="inline mr-2" /> <strong>View Therapists:</strong> Look for a new therapist.
+            <FaClipboardList className="inline mr-2" />{" "}
+            <strong>View Therapists:</strong> Look for a new therapist.
           </li>
           <li>
-            <FaFileInvoice className="inline mr-2" /> <strong>Invoices:</strong> View and manage your payment details.
+            <FaFileInvoice className="inline mr-2" /> <strong>Invoices:</strong>{" "}
+            View and manage your payment details.
           </li>
           <li>
-            <FaClipboardQuestion className="inline mr-2" /> <strong>Surveys:</strong> Answer weekly survey.
+            <FaClipboardQuestion className="inline mr-2" />{" "}
+            <strong>Surveys:</strong> Answer weekly survey.
           </li>
           <li>
-            <FaFileInvoice className="inline mr-2" /> <strong>Invoices:</strong> View and manage your payment details.
+            <FaFileInvoice className="inline mr-2" /> <strong>Invoices:</strong>{" "}
+            View and manage your payment details.
           </li>
         </ul>
         <div className="flex justify-center">
           <button
             className="mt-6 bg-[#5E9ED9] text-white px-4 py-2 rounded hover:bg-[#4a8ac9]"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Therapist List Popup
+const TherapistModal = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
+  const [message, setMessage] = useState<string>(""); // State for displaying the message
+  const [therapists, setTherapists] = useState<any[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [currentPage, setCurrentPage] = useState<number>(1); // Track current page
+  const itemsPerPage = 2; // Number of therapists to show per page
+
+  const handleSwitchRequest = (therapistName: string) => {
+    setMessage(`Switch Request Sent for ${therapistName}`); // Set the message with therapist's name
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      const fetchTherapists = async () => {
+        setLoading(true);
+        try {
+          setError(null); // Reset error on new request
+          const response = await fetch("/api/therapists/available");
+          if (!response.ok) {
+            throw new Error("Failed to fetch therapists");
+          }
+          const data = await response.json();
+          setTherapists(data.therapists); // Assuming the response structure has a 'therapists' field
+        } catch (err: any) {
+          setError(err.message || "An error occurred");
+        } finally {
+          setLoading(false); // Set loading to false once done
+        }
+      };
+
+      fetchTherapists();
+    }
+  }, [isOpen]);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentTherapists = therapists.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(therapists.length / itemsPerPage);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white rounded-lg p-6 w-full max-w-2xl outline outline-white outline-2 outline-offset-2">
+        <h2 className="text-3xl font-extrabold text-center text-[#5E9ED9] mb-4">
+          Available Therapists
+        </h2>
+        {loading && <p className="text-center text-gray-700"></p>}
+        {error && <p className="text-center text-red-500">{error}</p>}
+        {therapists.length === 0 && !loading && !error && (
+          <p className="text-center text-gray-700">
+            No therapists available at the moment.
+          </p>
+        )}
+        <ul className="text-gray-700 space-y-3">
+          {currentTherapists.map((therapist: any) => (
+            <li
+              key={therapist.id}
+              className="flex justify-between items-center mb-4"
+            >
+              <div>
+                <div className="flex items-center space-x-3 mt-2">
+                  <FaUser className="text-[#5E9ED9]" />
+                  <span className="font-medium">Therapist Name:</span>
+                  <span className="ml-2">
+                    {therapist.first_name} {therapist.last_name}
+                  </span>
+                </div>
+                <div className="flex items-center space-x-3 mt-2">
+                  <FaRestroom className="text-[#5E9ED9]" />
+                  <span className="font-medium">Gender:</span>
+                  <span className="ml-2">
+                    {therapist.gender.charAt(0).toUpperCase() +
+                      therapist.gender.slice(1)}
+                  </span>
+                </div>
+                <div className="flex items-center space-x-3 mt-2">
+                  <FaBriefcase className="text-[#5E9ED9]" />
+                  <span className="font-medium">Specialty:</span>
+                  <span className="ml-2">{therapist.specialization}</span>
+                </div>
+                <div className="flex items-center space-x-3 mt-2">
+                  <FaBookReader className="text-[#5E9ED9]" />
+                  <span className="font-medium">Years of Experience:</span>
+                  <span className="ml-2">{therapist.experience_years}</span>
+                </div>
+                <div className="flex items-center space-x-3 mt-2">
+                  <FaDollarSign className="text-[#5E9ED9]" />
+                  <span className="font-medium">Monthly Rate:</span>
+                  <span className="ml-2">${therapist.monthly_rate}</span>
+                </div>
+              </div>
+
+              <div className="ml-4 flex items-center">
+                <button
+                  className="bg-[#5E9ED9] text-white px-4 py-2 rounded hover:bg-[#4a8ac9] transition"
+                  onClick={() =>
+                    handleSwitchRequest(
+                      `${therapist.first_name} ${therapist.last_name}`
+                    )
+                  }
+                >
+                  <FaArrowRightArrowLeft className="inline mr-2" /> Switch
+                  Request
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex justify-center space-x-4 mt-4">
+          <button
+            className="bg-[#5E9ED9] text-white px-4 py-2 rounded hover:bg-[#4a8ac9]"
+            onClick={handlePreviousPage}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <span className="text-gray-700">
+            Page {currentPage} of {totalPages}
+          </span>
+          <button
+            className="bg-[#5E9ED9] text-white px-4 py-2 rounded hover:bg-[#4a8ac9]"
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+        </div>
+
+        {/* Empty space for message */}
+        <div className="text-center text-gray-700 mt-6">{message}</div>
+
+        <div className="flex justify-center mt-6">
+          <button
+            className="bg-[#5E9ED9] text-white px-4 py-2 rounded hover:bg-[#4a8ac9]"
             onClick={onClose}
           >
             Close
@@ -76,9 +255,13 @@ const TherapistSection = () => {
       </div>
 
       <div className="bg-blue-100 rounded-lg shadow-lg p-6 border border-[#5E9ED9]">
-        <h2 className="text-2xl text-center font-bold text-[#5E9ED9] mb-10">Your Therapist</h2>
+        <h2 className="text-2xl text-center font-bold text-[#5E9ED9] mb-10">
+          Your Therapist
+        </h2>
 
-        <p className="text-xl text-center font-medium text-gray-800 mb-10">[Therapist's Name]</p>
+        <p className="text-xl text-center font-medium text-gray-800 mb-10">
+          [Therapist's Name]
+        </p>
 
         <div className="mb-14">
           <button
@@ -95,15 +278,16 @@ const TherapistSection = () => {
           {isExpanded && (
             <div className="mt-4 flex space-x-5 justify-center text-gray-700">
               <p>
-                <FaUser className="inline mr-2 text-[#5E9ED9]" /> 
+                <FaUser className="inline mr-2 text-[#5E9ED9]" />
                 <span className="font-medium">Years of Experience:</span> 10
               </p>
               <p>
-                <FaBriefcase className="inline mr-2 text-[#5E9ED9]" /> 
-                <span className="font-medium">Specialty:</span> Anxiety & Depression
+                <FaBriefcase className="inline mr-2 text-[#5E9ED9]" />
+                <span className="font-medium">Specialty:</span> Anxiety &
+                Depression
               </p>
               <p>
-                <FaMapMarkerAlt className="inline mr-2 text-[#5E9ED9]" /> 
+                <FaMapMarkerAlt className="inline mr-2 text-[#5E9ED9]" />
                 <span className="font-medium">Location:</span> California
               </p>
             </div>
@@ -123,24 +307,24 @@ const TherapistSection = () => {
   );
 };
 
-// Menu Section Component
 const MenuSection = () => {
   const [isHelpOpen, setIsHelpOpen] = useState(false);
+  const [isTherListOpen, setIsTherListOpen] = useState(false);
 
   return (
     <div className="bg-blue-100 border border-[#5E9ED9] rounded-lg p-6 shadow-lg">
       <div className="items-center justify-between mb-6">
-        <h2 className="text-4xl text-center font-bold text-[#5E9ED9] mt-5">Menu</h2>
+        <h2 className="text-4xl text-center font-bold text-[#5E9ED9] mt-5">
+          Menu
+        </h2>
         <div className="flex justify-center md:mt-7 md:mb-20">
           <button
             className="bg-[#5E9ED9] text-white px-4 py-1 rounded-2xl hover:bg-[#4a8ac9] text-sm"
             onClick={() => setIsHelpOpen(true)}
           >
             <div className="flex justify-center space-x-2 p-1">
-              <div className="font-bold">
-                Help
-              </div> 
-              <FaQuestionCircle className="mt-0.5"/>
+              <div className="font-bold">Help</div>
+              <FaQuestionCircle className="mt-0.5" />
             </div>
           </button>
         </div>
@@ -149,7 +333,10 @@ const MenuSection = () => {
         <button className="w-full bg-[#5E9ED9] text-white px-6 py-4 text-lg font-semibold rounded hover:bg-[#4a8ac9] flex items-center justify-center">
           <FaBook className="mr-3" /> Journal
         </button>
-        <button className="w-full bg-[#5E9ED9] text-white px-6 py-4 text-lg font-semibold rounded hover:bg-[#4a8ac9] flex items-center justify-center">
+        <button
+          className="w-full bg-[#5E9ED9] text-white px-6 py-4 text-lg font-semibold rounded hover:bg-[#4a8ac9] flex items-center justify-center"
+          onClick={() => setIsTherListOpen(true)}
+        >
           <FaClipboardList className="mr-3" /> View Therapists
         </button>
         <button className="w-full bg-[#5E9ED9] text-white px-6 py-4 text-lg font-semibold rounded hover:bg-[#4a8ac9] flex items-center justify-center">
@@ -163,13 +350,16 @@ const MenuSection = () => {
         </button>
       </div>
       <HelpModal isOpen={isHelpOpen} onClose={() => setIsHelpOpen(false)} />
+      <TherapistModal
+        isOpen={isTherListOpen}
+        onClose={() => setIsTherListOpen(false)} // Pass studentId to fetch the therapist list
+      />
     </div>
   );
 };
 
 // Main Dashboard Component
 const StudentDashboard = () => {
-
   return (
     <div className="student-dashboard flex flex-col min-h-screen">
       <HeaderStudentDashboard />
--- frontend/src/pages/TherapistDashboard.tsx
+++ frontend/src/pages/TherapistDashboard.tsx
@@ -1,45 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import HeaderTherapistDashboard from "../components/HeaderTherapistDashboard";
 import Footer from "../components/Footer";
 import {
-  FaCalendarAlt, 
-  FaUser, 
+  FaCalendarAlt,
+  FaUser,
   FaMapMarkerAlt,
-  FaComments, 
-  FaQuestionCircle, 
-  FaFileInvoice, 
-  FaUserPlus, 
-  FaTasks
+  FaComments,
+  FaQuestionCircle,
+  FaFileInvoice,
+  FaUserPlus,
+  FaTasks,
 } from "react-icons/fa";
 
-import Lottie from 'lottie-react';
-import TherapistDashboardAnimation from '../assets/lotties/TherapistDashboardAnimation.json';
-import Switch from '@mui/material/Switch';
+import Lottie from "lottie-react";
+import TherapistDashboardAnimation from "../assets/lotties/TherapistDashboardAnimation.json";
+import Switch from "@mui/material/Switch";
 
 // Help Modal Component
-const HelpModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
+const HelpModal = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white rounded-lg p-6 w-full max-w-md outline outline-white outline-2 outline-offset-2">
-        <h2 className="text-3xl font-extrabold text-center text-[#5E9ED9] mb-4">Help</h2>
+        <h2 className="text-3xl font-extrabold text-center text-[#5E9ED9] mb-4">
+          Help
+        </h2>
         <ul className="text-gray-700 space-y-3">
           <li>
-            <FaUser className="inline mr-2" /> <strong>Manage Patients:</strong> Access and update patient records, chats, and notes.
+            <FaUser className="inline mr-2" /> <strong>Manage Patients:</strong>{" "}
+            Access and update patient records, chats, and notes.
           </li>
           <li>
-            <FaCalendarAlt className="inline mr-2" /> <strong>View Appointments:</strong> Manage your calendar and upcoming sessions.
+            <FaCalendarAlt className="inline mr-2" />{" "}
+            <strong>View Appointments:</strong> Manage your calendar and
+            upcoming sessions.
           </li>
           <li>
-            <FaComments className="inline mr-2" /> <strong>New Patient Requests:</strong> Review and accept new patient inquiries.
+            <FaComments className="inline mr-2" />{" "}
+            <strong>New Patient Requests:</strong> Review and accept new patient
+            inquiries.
           </li>
           <li>
-            <FaFileInvoice className="inline mr-2" /> <strong>Invoices:</strong> View and manage billing for your services.
+            <FaFileInvoice className="inline mr-2" /> <strong>Invoices:</strong>{" "}
+            View and manage billing for your services.
           </li>
         </ul>
         <div className="flex justify-center">
-          <button className="mt-6 bg-[#5E9ED9] text-white px-4 py-2 rounded hover:bg-[#4a8ac9]" onClick={onClose}>Close</button>
+          <button
+            className="mt-6 bg-[#5E9ED9] text-white px-4 py-2 rounded hover:bg-[#4a8ac9]"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Patient Request Modal Component
+const RequestModal = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white rounded-lg p-6 w-full max-w-md outline outline-white outline-2 outline-offset-2">
+        <h2 className="text-3xl font-extrabold text-center text-[#5E9ED9] mb-4">
+          Patient Requests
+        </h2>
+        <div className="flex justify-center">
+          <button
+            className="mt-6 bg-[#5E9ED9] text-white px-4 py-2 rounded hover:bg-[#4a8ac9]"
+            onClick={onClose}
+          >
+            Close
+          </button>
         </div>
       </div>
     </div>
@@ -50,19 +98,30 @@ const PatientDetailsPopup = ({ onClose, patient }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="relative bg-white rounded-lg p-10 w-full max-w-xl outline outline-white outline-2 outline-offset-2">
-        <button onClick={onClose} className="absolute top-0 right-0 text-black text-lg p-2 m-2">
+        <button
+          onClick={onClose}
+          className="absolute top-0 right-0 text-black text-lg p-2 m-2"
+        >
           &#x2715;
         </button>
-        <h2 className="text-3xl font-bold text-center text-[#5E9ED9]">{patient.name}</h2>
+        <h2 className="text-3xl font-bold text-center text-[#5E9ED9]">
+          {patient.name}
+        </h2>
         <p className="text-gray-700 mt-4">
-          Patient details: Lorem ipsum dolor sit amet, 
-          consectetur adipiscing elit. Praesent mattis ex ac laoreet tempus. Vestibulum sapien 
-          ligula, venenatis et orci in, auctor feugiat massa.
+          Patient details: Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Praesent mattis ex ac laoreet tempus. Vestibulum sapien ligula,
+          venenatis et orci in, auctor feugiat massa.
         </p>
         <div className="flex justify-around mt-4">
-          <button className="bg-[#5E9ED9] text-white px-4 py-2 rounded-full hover:bg-[#4a8ac9]">Records</button>
-          <button className="bg-[#5E9ED9] text-white px-4 py-2 rounded-full hover:bg-[#4a8ac9]">Chat</button>
-          <button className="bg-[#5E9ED9] text-white px-4 py-2 rounded-full hover:bg-[#4a8ac9]">Notes</button>
+          <button className="bg-[#5E9ED9] text-white px-4 py-2 rounded-full hover:bg-[#4a8ac9]">
+            Records
+          </button>
+          <button className="bg-[#5E9ED9] text-white px-4 py-2 rounded-full hover:bg-[#4a8ac9]">
+            Chat
+          </button>
+          <button className="bg-[#5E9ED9] text-white px-4 py-2 rounded-full hover:bg-[#4a8ac9]">
+            Notes
+          </button>
         </div>
       </div>
     </div>
@@ -72,8 +131,15 @@ const PatientDetailsPopup = ({ onClose, patient }) => {
 // Lottie Animation Component
 const LottieAnimation = () => {
   return (
-    <div className="bg-blue-100 rounded-lg shadow-lg mb-4 border border-[#5E9ED9] p-2" style={{ height: '300px' }}>
-      <Lottie animationData={TherapistDashboardAnimation} loop={true} style={{ width: '100%', height: '100%' }} />
+    <div
+      className="bg-blue-100 rounded-lg shadow-lg mb-4 border border-[#5E9ED9] p-2"
+      style={{ height: "300px" }}
+    >
+      <Lottie
+        animationData={TherapistDashboardAnimation}
+        loop={true}
+        style={{ width: "100%", height: "100%" }}
+      />
     </div>
   );
 };
@@ -81,18 +147,23 @@ const LottieAnimation = () => {
 // Patient Section Component
 const PatientSection = () => {
   const [selectedPatient, setSelectedPatient] = useState(null);
-  
+
   const patients = Array.from({ length: 5 }, (_, i) => ({
     id: i,
-    name: `FirstName LastName ${i + 1}`
+    name: `FirstName LastName ${i + 1}`,
   }));
 
   return (
     <div className="bg-blue-100 rounded-lg shadow-lg p-6 border border-[#5E9ED9]">
-      <h2 className="text-2xl text-center font-bold text-[#5E9ED9] mb-4">Patients</h2>
+      <h2 className="text-2xl text-center font-bold text-[#5E9ED9] mb-4">
+        Patients
+      </h2>
       <div>
         {patients.map((patient) => (
-          <div key={patient.id} className="flex justify-between items-center p-2 border-b border-gray-300">
+          <div
+            key={patient.id}
+            className="flex justify-between items-center p-2 border-b border-gray-300"
+          >
             <span>Patient: {patient.name}</span>
             <button
               className="bg-[#5E9ED9] text-white px-4 py-1 rounded hover:bg-[#4a8ac9]"
@@ -104,8 +175,8 @@ const PatientSection = () => {
         ))}
       </div>
       {selectedPatient && (
-        <PatientDetailsPopup 
-          patient={selectedPatient} 
+        <PatientDetailsPopup
+          patient={selectedPatient}
           onClose={() => setSelectedPatient(null)}
         />
       )}
@@ -117,6 +188,7 @@ const PatientSection = () => {
 const TherapistDashboard = () => {
   const [isHelpOpen, setIsHelpOpen] = useState(false);
   const [isAvailable, setIsAvailable] = useState(true);
+  const [isRequestOpen, setIsRequestOpen] = useState(false);
 
   const toggleAvailability = () => {
     setIsAvailable(!isAvailable);
@@ -136,7 +208,9 @@ const TherapistDashboard = () => {
           <PatientSection />
         </div>
         <div className="col-span-1 bg-blue-100 rounded-lg shadow-lg p-6 border border-[#5E9ED9]">
-          <h2 className="text-4xl text-center font-bold text-[#5E9ED9]">Menu</h2>
+          <h2 className="text-4xl text-center font-bold text-[#5E9ED9]">
+            Menu
+          </h2>
           <div className="space-y-4 mt-4">
             <div className="flex items-center justify-center space-x-2">
               <span>Not Available</span>
@@ -144,19 +218,20 @@ const TherapistDashboard = () => {
               <span>Available</span>
             </div>
             <div className="flex justify-center md:mt-7 md:mb-20">
-          <button
-            className="bg-[#5E9ED9] text-white px-4 py-1 rounded-2xl hover:bg-[#4a8ac9] text-sm"
-            onClick={() => setIsHelpOpen(true)}
-          >
-            <div className="flex justify-center space-x-2 p-1">
-              <div className="font-bold">
-                Help
-              </div> 
-              <FaQuestionCircle className="mt-0.5"/>
+              <button
+                className="bg-[#5E9ED9] text-white px-4 py-1 rounded-2xl hover:bg-[#4a8ac9] text-sm"
+                onClick={() => setIsHelpOpen(true)}
+              >
+                <div className="flex justify-center space-x-2 p-1">
+                  <div className="font-bold">Help</div>
+                  <FaQuestionCircle className="mt-0.5" />
+                </div>
+              </button>
             </div>
-          </button>
-        </div>
-            <button className="w-full bg-[#5E9ED9] text-white px-6 py-4 text-lg font-semibold rounded hover:bg-[#4a8ac9] flex items-center justify-center">
+            <button
+              className="w-full bg-[#5E9ED9] text-white px-6 py-4 text-lg font-semibold rounded hover:bg-[#4a8ac9] flex items-center justify-center"
+              onClick={() => setIsRequestOpen(true)}
+            >
               <FaUserPlus className="mr-3" /> View New Patient Requests
             </button>
             <button className="w-full bg-[#5E9ED9] text-white px-6 py-4 text-lg font-semibold rounded hover:bg-[#4a8ac9] flex items-center justify-center">
@@ -170,6 +245,10 @@ const TherapistDashboard = () => {
       </div>
       <Footer />
       <HelpModal isOpen={isHelpOpen} onClose={() => setIsHelpOpen(false)} />
+      <RequestModal
+        isOpen={isRequestOpen}
+        onClose={() => setIsRequestOpen(false)}
+      />
     </div>
   );
 };
