bd9aac9fca224ba1e739c31ae52f77bf633fa0a5
Steven-Alvarado <Steven67alvarado@gmail.com>
2024-12-11 12:24:23 -0500

    Added PATCH to allowed methods in CORS

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true, // Allow credentials
   },
 });
